b01d8c7f9fab10dacb64b9f014a0ea38c784ffb7
adnanmahmud0 <adnan.mahmud.pc@gmail.com>
2025-02-13 01:36:56 +0600

    nav bar dark mood added and login page design updated

--- src/component/auth/Login.jsx
+++ src/component/auth/Login.jsx
@@ -5,6 +5,7 @@ import Swal from 'sweetalert2';
 import { useContext, useState } from 'react';
 import { TbPassword } from "react-icons/tb";
 import { AuthContext } from '../authProvider/AuthProvider';
+import login from "../../assets/login.png";
 
 const Login = () => {
     const { loginUser, loginWithGoogle } = useContext(AuthContext);
@@ -94,34 +95,34 @@ const Login = () => {
             <div className="">
                 <div className="min-h-screen flex flex-col items-center justify-center py-6 px-4">
                     <div className="grid md:grid-cols-2 items-center gap-4 max-w-6xl w-full">
-                        <div className="border border-gray-300 rounded-lg p-6 max-w-md shadow-[0_2px_22px_-4px_rgba(93,96,127,0.2)] max-md:mx-auto">
+                        <div className="border border-[] rounded-lg p-6 max-w-md shadow-[0_2px_22px_-4px_rgba(93,96,127,0.2)] max-md:mx-auto">
                             <form onSubmit={handleLogin} className="space-y-4">
                                 <div className="mb-8">
-                                    <h3 className="text-gray-800 text-3xl font-extrabold">Sign in</h3>
+                                    <h3 className="text-[#02C5BC] text-3xl font-extrabold">Sign in</h3>
                                     <p className="text-gray-500 text-sm mt-4 leading-relaxed">
                                         Sign in to your account and explore a world of possibilities. Your journey begins here.
                                     </p>
                                 </div>
 
                                 <div>
-                                    <label className="text-gray-800 text-sm mb-2 block">Email</label>
+                                    <label className="text-[#02C5BC] text-sm mb-2 block">Email</label>
                                     <div className="relative flex items-center">
                                         <input
                                             name="email"
                                             type="email"
-                                            className="w-full text-sm text-gray-800 border border-gray-300 px-4 py-3 rounded-lg outline-blue-600"
+                                            className="w-full text-sm text-gray-800 border border-gray-300 px-4 py-3 rounded-lg outline-[#02C5BC]"
                                             placeholder="Enter user email"
                                         />
                                         <IoIosMail className="absolute right-4 text-gray-400" />
                                     </div>
                                 </div>
                                 <div>
-                                    <label className="text-gray-800 text-sm mb-2 block">Password</label>
+                                    <label className="text-[#02C5BC] text-sm mb-2 block">Password</label>
                                     <div className="relative flex items-center">
                                         <input
                                             name="password"
                                             type={showPassword ? "text" : "password"}
-                                            className="w-full text-sm text-gray-800 border border-gray-300 px-4 py-3 rounded-lg outline-blue-600"
+                                            className="w-full text-sm text-gray-800 border border-gray-300 px-4 py-3 rounded-lg outline-[#02C5BC]"
                                             placeholder="Enter password"
                                         />
                                         <TbPassword className="absolute right-4 text-gray-400" />
@@ -133,7 +134,7 @@ const Login = () => {
                                         <input
                                             onClick={togglePassword}
                                             type="checkbox"
-                                            className="h-4 w-4 shrink-0 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+                                            className="h-4 w-4 shrink-0 text-[#02C5BC] focus:ring-[#02C5BC] border-gray-300 rounded"
                                         />
                                         <label  htmlFor="remember-me" className="ml-3 block text-sm text-gray-800">
                                             Show Password
@@ -141,7 +142,7 @@ const Login = () => {
                                     </div>
 
                                     <div className="text-sm">
-                                        <Link to="/forget-password" className="text-blue-600 hover:underline font-semibold">
+                                        <Link to="/forget-password" className="text-[#02C5BC] hover:underline font-semibold">
                                             Forgot your password?
                                         </Link>
                                     </div>
@@ -150,19 +151,19 @@ const Login = () => {
                                 <div className="!mt-8">
                                     <button
                                         type="submit"
-                                        className="w-full shadow-xl py-3 px-4 text-sm tracking-wide rounded-lg text-white bg-blue-600 hover:bg-blue-700 focus:outline-none"
+                                        className="w-full py-3 px-4 text-sm tracking-wide rounded-lg text-[#02C5BC] border border-[#02C5BC] bg-transparent hover:bg-[#02C5BC] hover:text-white focus:outline-none"
                                     >
                                         Log in
                                     </button>
                                 </div>
-                                <div className="text-center text-gray-800 text-sm mt-4">
+                                <div className="text-center text-[#363A76] text-sm mt-4">
                                     OR
                                 </div>
                                 <div>
                                     <button
                                         onClick={handleGoogleLogin}
                                         type="button"
-                                        className="w-full shadow-xl py-3 px-4 text-sm tracking-wide rounded-lg text-white bg-blue-600 hover:bg-blue-700 focus:outline-none"
+                                        className="w-full py-3 px-4 text-sm tracking-wide rounded-lg text-[#FACC01] border border-[#FACC01] bg-transparent hover:bg-[#FACC01] hover:text-white focus:outline-none"
                                     >
                                         <FaGoogle className="inline-block mr-2" />
                                         Sign in with Google
@@ -171,15 +172,15 @@ const Login = () => {
 
                                 <p className="text-sm !mt-8 text-center text-gray-800">
                                     Don't have an account{' '}
-                                    <Link to="/register" className="text-blue-600 font-semibold hover:underline ml-1 whitespace-nowrap">
+                                    <Link to="/register" className="text-[#FACC01] font-semibold hover:underline ml-1 whitespace-nowrap">
                                         Register here
                                     </Link>
                                 </p>
                             </form>
                         </div>
-                        <div className="lg:h-[400px] md:h-[300px] max-md:mt-8">
+                        <div className="">
                             <img
-                                src="https://readymadeui.com/login-image.webp"
+                                src={login}
                                 className="w-full h-full max-md:w-4/5 mx-auto block object-cover"
                                 alt="Dining Experience"
                             />
--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,114 +1,114 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../authProvider/AuthProvider";
-import  WhereIsIt  from "../../assets/whereIsIt.png";
+import WhereIsIt from "../../assets/lost and found.png";
 import Swal from 'sweetalert2';
-
+import { FaSun, FaMoon } from 'react-icons/fa'; // React Icons for Sun and Moon
 
 const Navbar = () => {
-
     const { user, logOut } = useContext(AuthContext);
     const navigate = useNavigate();
 
-    const link = <>
-        <li><Link to="/" className="">Home</Link></li>
-        <li><Link to="/lost-and-found" className="">Lost & Found</Link></li>
-        <li><Link to="/contact" className="">Contact</Link></li>
-    </>
+    // Theme state: Default to light mode
+    const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+    useEffect(() => {
+        document.documentElement.setAttribute("data-theme", theme);
+        localStorage.setItem("theme", theme);
+    }, [theme]);
+
+    // Function to toggle theme (light → dark)
+    const toggleTheme = () => {
+        const newTheme = theme === "light" ? "dark" : "light";
+        setTheme(newTheme);
+    };
 
     const handelLogout = () => {
         logOut()
-        .then(() => {
-            Swal.fire({
-              title: "Good job!",
-              text: "Logged out successfully!",
-              icon: "success",
-            });
-            navigate("/");
-          })
-          .catch((error) => Swal.fire("Error", error.message, "error"));
+            .then(() => {
+                Swal.fire({
+                    title: "Good job!",
+                    text: "Logged out successfully!",
+                    icon: "success",
+                });
+                navigate("/");
+            })
+            .catch((error) => Swal.fire("Error", error.message, "error"));
+    };
 
-    }
+    
+    const link = <>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/lost-and-found">Lost & Found</Link></li>
+        <li><Link to="/all-recoverd-items">All Recovered Items Page</Link></li>
+    </>;
 
+    const linkall = <>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/lost-and-found">Lost & Found</Link></li>
+        <li><Link to="/all-recoverd-items">All Recovered Items Page</Link></li>
+        <li>
+            <button className="btn btn-ghost " onClick={toggleTheme}>
+                {theme === "light" ? <FaSun className="text-balck" size={24} /> : <FaMoon className="text-white" size={24} />}
+            </button>
+        </li>
+    </>;
 
     return (
-        <div className="">
+        <div className="fixed w-full z-50 shadow-md bg-base-100">
             <div className="navbar max-w-7xl mx-auto">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor">
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M4 6h16M4 12h8m-8 6h16" />
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
                             </svg>
                         </div>
-                        <ul className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">{link}
-                        </ul>
+                        <ul className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">{linkall}</ul>
                     </div>
                     <div className="flex items-center space-x-2">
-                        <img src={WhereIsIt} alt="" className="w-10 h-10" />
-                        <a className="font-extrabold text-lg">WhereIsIt</a>
+                        <img src={WhereIsIt} alt="" className="w-7 h-8" />
+                        <a className="font-extrabold text-lg md:flex hidden"><span>Where</span><>Is</><span>It</span></a>
                     </div>
-                    
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        {link}
-                    </ul>
+                    <ul className="menu menu-horizontal px-1">{link}</ul>
                 </div>
                 <div className="navbar-end space-x-5">
+                    {/* Theme Toggle Button */}
+                    <button className="btn btn-ghost md:flex hidden " onClick={toggleTheme}>
+                        {theme === "light" ? <FaSun className="text-balck" size={24} /> : <FaMoon className="text-white" size={24} />}
+                    </button>
+
                     {user ? (
                         <div className="flex items-center space-x-5">
                             <div className="dropdown dropdown-end relative group">
-                                <div
-                                    tabIndex={0}
-                                    role="button"
-                                    className="btn btn-ghost btn-circle avatar"
-                                >
+                                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                                     <div className="w-10 rounded-full">
-                                        <img
-                                            alt={user.displayName}
-                                            src={user.photoURL}
-                                        />
+                                        <img alt={user.displayName} src={user.photoURL} />
                                     </div>
                                 </div>
-
                                 <span className="absolute top-12 left-1/2 -translate-x-1/2 scale-0 rounded bg-gray-800 p-2 text-sm text-white opacity-0 shadow-lg transition-all duration-300 group-hover:scale-100 group-hover:opacity-100">
                                     {user.displayName}
                                 </span>
-
-                                <ul
-                                    tabIndex={0}
-                                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-60 p-2 shadow"
-                                >
+                                <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-60 p-2 shadow">
                                     <li><Link to="/add-items">Add Lost & Found Item Page</Link></li>
-                                    <li><Link to={`/all-recoverd-items`}>All Recovered Items Page</Link></li>
-                                    <li><Link to={`/my-items`}>Manage My Items Page</Link></li>
+                                    <li><Link to="/my-items">Manage My Items Page</Link></li>
                                 </ul>
                             </div>
                             <div>
-                                <a className="btn" onClick={handelLogout}>Logout</a>
+                                <a className="btn border border-[#02C5BC] bg-transparent text-[#02C5BC] hover:bg-[#02C5BC] hover:text-white" onClick={handelLogout}>Logout</a>
                             </div>
                         </div>
-
                     ) : (
                         <>
                             <div>
-                                <Link className="btn" to="/login">Login</Link>
+                                <Link className="btn border border-[#F4BE32] bg-transparent text-[#F4BE32] hover:bg-[#F4BE32] hover:text-white" to="/login">Login</Link>
                             </div>
                             <div>
-                                <Link className="btn" to="/register">Register</Link>
+                                <Link className="btn border border-[#F4BE32] bg-transparent text-[#F4BE32] hover:bg-[#F4BE32] hover:text-white" to="/register">Register</Link>
                             </div>
                         </>
-
                     )}
                 </div>
             </div>
